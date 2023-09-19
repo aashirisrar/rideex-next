4dd2034ff29276a6f98acb1c5bd28bd5bd5f5885
Aashir Israr <63807168+aashirisrar@users.noreply.github.com>
2023-09-20 02:23:15 +0500

    completed the footer section

--- components/landing-footer.tsx
+++ components/landing-footer.tsx
@@ -1,7 +1,38 @@
+import Image from "next/image";
 import React from "react";
+import logo from "@/public/logo.png";
 
 const LandingFooter = () => {
-  return <div>LandingFooter</div>;
+  return (
+    <div className="h-[24rem] pt-20 px-16 bg-white text-black font-semibold">
+      <div className="flex col-span-4 gap-12 justify-center">
+        <div className="mt-16">
+          <Image width="194" src={logo} alt="logo" />
+        </div>
+        <div>
+          <div className="my-8">Navigation</div>
+          <div>Home</div>
+          <div>About</div>
+          <div>Menu</div>
+        </div>
+        <div>
+          <div className="my-8">Contact</div>
+          <div>Address</div>
+          <div>Phone Number</div>
+          <div>Email</div>
+        </div>
+        <div>
+          <div className="my-8">Social Media Links</div>
+          <div>Twitter</div>
+          <div>Facebook</div>
+          <div>Instagram</div>
+        </div>
+      </div>
+      <div className="text-center font-normal mt-16">
+        Copyright @2023 RideEx. All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default LandingFooter;
